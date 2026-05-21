7f4735edcd74cfdd835e1e43b535f9edf856f9d6
dykesimulator <dykesimulator@gmail.com>
2026-05-21 23:17:39 +0800

    we tried adding party classes

--- ally/party_data.gd
+++ ally/party_data.gd
@@ -4,4 +4,3 @@ class_name partyData
 @export var name: String = "ally"
 @export var texture: Texture2D;
 @export var max_health: int
-@export var damage: int
---
+++ ally/sentinels.gd
@@ -0,0 +1,4 @@
+extends partyData
+class_name sentinels
+
+@export var damage: int
